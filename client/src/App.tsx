9754ae0a75740e9a295d3c0f1a7e885f734e1c4a
unknown <jo.24family@gmail.com>
2026-01-14 01:34:31 +0900

    Fix: App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Router, Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
 
-function Router() {
+function Routes() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
@@ -32,7 +32,9 @@ function App() {
       >
         <TooltipProvider>
           <Toaster position="bottom-left" />
-          <Router />
+          <Router base={import.meta.env.BASE_URL}>
+            <Routes />
+          </Router>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
